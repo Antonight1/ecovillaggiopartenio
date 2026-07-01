a2979947f7771927dd9751e4c96d286a4d7b6b66
Antonio Bollito <bollitoantonio09@gmail.com>
2026-07-01 12:48:56 +0200

    Add Cloudinary image upload and optimization script
    This script configures Cloudinary, uploads an image, and generates optimized and transformed URLs.

---
+++ idex.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dfd44st4u', 
+        api_key: '129565636249355', 
+        api_secret: '<r1NcMAJWeu1GUPco_EHkHZ022z8>' // Click 'View API Keys' above to copy your API secret
+    });
+    
+    // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+})();
