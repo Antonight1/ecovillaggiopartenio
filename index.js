3d2bdaacebc7d8a3cb3afd60e8503496cb220d00
Antonio Bollito <bollitoantonio09@gmail.com>
2026-07-10 12:14:37 +0200

    Update index.js
    removed api keys

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: 'dfd44st4u',
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
